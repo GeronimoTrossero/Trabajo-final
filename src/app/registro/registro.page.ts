facb3c3313913eb5ddc3a6a1980d03af454fdd03
unknown <lubasabe164@gmail.com>
2022-11-07 16:32:31 -0300

    Lu

---
+++ src/app/registro/registro.page.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import{
+  FormGroup,
+  FormControl,
+  Validators,
+  FormBuilder
+} from "@angular/forms";
+import { AlertController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-registro',
+  templateUrl: './registro.page.html',
+  styleUrls: ['./registro.page.scss'],
+})
+export class RegistroPage implements OnInit {
+
+  formularioRegistro: FormGroup;
+
+  constructor(public fb: FormBuilder,
+    public alertController: AlertController) { 
+    this.formularioRegistro = this.fb.group({
+      "nombre" : new FormControl("",Validators.required),
+      "password" : new FormControl("",Validators.required),
+      "confirmacionPassword" : new FormControl("",Validators.required)
+    });
+  }
+
+  ngOnInit() {
+  }
+
+  async guardar(){
+    var f = this.formularioRegistro.value;
+
+    if(this.formularioRegistro.invalid){
+      const alert = await this.alertController.create({
+        message: "Todos los campos son obligatorios",
+        buttons: ["Aceptar"]
+      });
+
+      await alert.present();
+      return;
+    }
+  }
+
+}
